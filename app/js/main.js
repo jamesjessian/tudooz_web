e7a9af70d4c5a7aa2f06c41a43b40c9b4497f949
James Robinson <james.robinson@coraltreesystems.com>
2016-09-25 20:43:35 +0100

    Ensure that front page isn't shown again after authentication redirect.

--- app/js/main.js
+++ app/js/main.js
@@ -19,50 +19,39 @@ require(["jquery"], function($) {
 		window.lock = null;
 		// When page has loaded...
 		$(document).ready(function() {
-			// Get new Auth0 lock object.  This may trigger 'authenticated' event if 
-			// user has been authenticated and redirected back here.
-			lock = new Auth0Lock('fFIlD3qHLFbEISojQKN3LAG4jY8O0mKy', 'tudooz.eu.auth0.com', {
-					auth: { 
-							params: { scope: 'openid email' } //Details: https:///scopes
-					},
-					allowedConnections: ['facebook', 'twitter'],
-					socialButtonStyle: 'big',
-					languageDictionary: {
-						title: ""
-					},
-					rememberLastLogin: true,
-					theme: {
-						logo: 'img/logo.png'
-					}  
+			// Initialise Auth0
+			var auth0 = new Auth0({
+				domain:       'tudooz.eu.auth0.com',
+				clientID:     'fFIlD3qHLFbEISojQKN3LAG4jY8O0mKy',
+				responseType: 'token'
 			});
 
-			// Check if 'idToken' exists in local storage to indicate open session
-			var idToken = localStorage.getItem('id_token');
-			if(!idToken) {
-				displayFront();
-			}
-			else {
+			// Check if we have just received callback token after authentication
+			// OR 'idToken' exists in local storage to indicate open session
+			var idToken = null;
+			var result = auth0.parseHash(window.location.hash);
+			if (result && result.idToken)
+				idToken = result.idToken;
+			else
+				idToken = localStorage.getItem('id_token');
+
+			if (idToken) {
 				// Check token is still valid
-				lock.getProfile(idToken, function(error, profile) {
-					if (error) {
+				auth0.getProfile(idToken, function(error, profile) {
+					if (error)
 						displayFront();
-					} 
 					else {
+						localStorage.setItem('id_token', idToken);
+						localStorage.setItem('profile', JSON.stringify(profile));
 						loadMain();
 					}
 				});
+			} else if (result && result.error) {
+				console.error('Error at login: ' + result.error)
+				displayFront();
+			} else {
+				displayFront();
 			}
-
-			lock.on("authenticated", function(authResult) {
-				lock.getProfile(authResult.idToken, function(error, profile) {
-					if (error) {
-						return;
-					}
-					localStorage.setItem('id_token', authResult.idToken);
-					localStorage.setItem('profile', JSON.stringify(profile));
-					loadMain();
-				});
-			});
 		});
 });
 
@@ -71,12 +60,30 @@ require(["jquery"], function($) {
  * Used to display initial front screen.
  */
 function displayFront() {
+
+	// Get new Auth0 lock object.  This may trigger 'authenticated' event if 
+	// user has been authenticated and redirected back here.
+	var lock = new Auth0Lock('fFIlD3qHLFbEISojQKN3LAG4jY8O0mKy', 'tudooz.eu.auth0.com', {
+			auth: { 
+					params: { scope: 'openid email' } //Details: https:///scopes
+			},
+			allowedConnections: ['facebook', 'twitter'],
+			socialButtonStyle: 'big',
+			languageDictionary: {
+				title: ""
+			},
+			rememberLastLogin: true,
+			theme: {
+				logo: 'img/logo.png'
+			}  
+	});
 	$(".front").show();
 	$(".login-button").click(function(){ lock.show() });
 }
 
 /**
- * 
+ * loadMain
+ *  Render main todo lists screen
  */
 function loadMain() {
 	require(["react", "jsx!/views/all-lists"], function(React, AllLists) {
